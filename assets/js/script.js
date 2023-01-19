59ce6d281549c6d51d0556aa8e62c2c0125384cc
Luan Cordeiro <luam.lc19@gmail.com>
2023-01-19 16:23:36 -0300

    create script and variables for interface control

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// interface control
+let yourVoteFor = document.querySelector('.screen-top--left .left-1 span');
+let role = document.querySelector('.screen-top--left .left-2');
+let numbers = document.querySelector('.screen-top--left .left-3');
+let description = document.querySelector('.screen-top--left .left-4');
+let warming = document.querySelector('.screen .screen-bottom p');
+let aside = document.querySelector('.screen-top .screen-top--right');
